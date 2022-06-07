5bde7257d135fb08cd95960f0077d7619f05d5fc
amitsethii <amitsethi150@gmail.com>
2022-06-07 20:56:01 +0530

    Add files via upload

---
+++ todo-list (12/app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.set('view engine','ejs');
+
+var items=["Buy Food","Cook Food","Eat Food"];
+
+var workItems = ["Baked"];
+
+app.get("/",function(req,res){
+
+var today = new Date();
+
+var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
+
+var day = today.toLocaleDateString("en-US",options);
+
+res.render("list",{ listTitle: day, newListItems:items});
+});
+
+app.post("/",function(req,res){
+console.log(req.body);
+    var item = req.body.newItem;
+
+if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+
+}else{
+    items.push(item);
+    res.redirect("/");
+}
+
+});
+
+app.get("/work", function(req,res){
+
+    res.render("list",{listTitle: "Work List", newListItems:workItems });
+
+});
+
+// app.post("/work", function(req,res){
+
+//     var item = req.body.newItem;
+
+//     workItems.push(item);
+
+//     res.redirect("/work");
+
+// });
+
+
+app.listen(3000,function(){
+
+    console.log("server started");
+
+});
+
+app.get("/about",function(req,res){
+
+res.render("about");
+
+});
+
+
+
+
+// THEMPLATE VS LAYOUT(header footer)
+//we added work which is template of our project 
