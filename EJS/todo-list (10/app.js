33ca44b8e29adc1bb8a181fa1db9206b4f4bf347
amitsethii <amitsethi150@gmail.com>
2022-06-26 13:02:16 +0530

    Add files via upload

---
+++ EJS/todo-list (10/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+var items=["Buy Food","Cook Food","Eat Food"];
+app.set('view engine','ejs');
+
+
+app.get("/",function(req,res){
+   
+    var today = new Date();
+    var options ={
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+  var day = today.toLocaleDateString("en-US",options);
+
+    res.render("list",{kindOfDay: day, newListItems:items});
+
+});
+
+
+app.post("/",function(req,res){
+   var  item = req.body.newItem;
+   items.push(item);
+   res.redirect("/");
+})
+
+
+
+app.listen(3000,function(){
+    console.log("server running");
+});
+
